d0669187e6dfc8de26d11d851f996e6e291b289a
Miguel Bento <miguel.bento@eurail.com>
2020-02-28 21:28:32 +0100

    Updates

---
+++ precache-manifest.7b6247c7fca56d9c82146504aae1251a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d874481e83d06243345228091c6c94",
+    "url": "/photo-gallery/index.html"
+  },
+  {
+    "revision": "364daba92e00e199a6d7",
+    "url": "/photo-gallery/static/css/3.66ba874d.chunk.css"
+  },
+  {
+    "revision": "34b6693c24b2d1028339",
+    "url": "/photo-gallery/static/css/main.733f1e84.chunk.css"
+  },
+  {
+    "revision": "3f05c60fab653286ef22",
+    "url": "/photo-gallery/static/js/2.389ca9f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/photo-gallery/static/js/2.389ca9f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "364daba92e00e199a6d7",
+    "url": "/photo-gallery/static/js/3.060e3f34.chunk.js"
+  },
+  {
+    "revision": "34b6693c24b2d1028339",
+    "url": "/photo-gallery/static/js/main.b44b0ebd.chunk.js"
+  },
+  {
+    "revision": "bf0dbe0e5a3bbead0018",
+    "url": "/photo-gallery/static/js/runtime-main.718ed5d4.js"
+  },
+  {
+    "revision": "2df31d1ef0c8a3cef879390a6f880363",
+    "url": "/photo-gallery/static/media/minus.2df31d1e.svg"
+  },
+  {
+    "revision": "94c9238b080fbd85791416ed2f06ae47",
+    "url": "/photo-gallery/static/media/plus.94c9238b.svg"
+  },
+  {
+    "revision": "64b794c352ce5c73232bfc91acb6b758",
+    "url": "/photo-gallery/static/media/search.64b794c3.svg"
+  }
+]);
